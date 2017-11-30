46a1de4044d94866283654c87ce7ce80e6636b57
Nikolay <ndmoroz@gmail.com>
2017-12-01 02:12:37 +0300

    Add click-on-image pixelizing a part of image

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from PyQt5.QtWidgets import \
     QLabel, \
     QWidget, \
     QApplication, \
-    QFileDialog, \
-    QMessageBox
+    QFileDialog
 from PyQt5.QtGui import QPixmap
 from PIL import Image, ImageDraw
 from PIL.ImageQt import ImageQt
@@ -13,15 +12,12 @@ import sys
 
 
 class ClickableLabel(QLabel):
-    def __init__(self):
+    def __init__(self, callable):
         super().__init__()
+        self.callable = callable
 
     def mouseReleaseEvent(self, QMouseEvent):
-        QMessageBox.question(self, 'Message',
-                             'PyQt5 message',
-                             QMessageBox.Yes,
-                             QMessageBox.No)
-        return QMouseEvent.pos()
+        self.callable(QMouseEvent.x(), QMouseEvent.y())
 
 
 class PicWindow(QWidget):
@@ -29,15 +25,12 @@ class PicWindow(QWidget):
         super().__init__()
 
         image_path = self.getfilename()
-        image = Image.open(image_path)
-        img_tmp = ImageQt(image.convert('RGBA'))
-        pixmap = QPixmap.fromImage(img_tmp)
-
-        lbl = ClickableLabel()
-        lbl.setPixmap(pixmap)
+        self.image = Image.open(image_path)
+        self.lbl = ClickableLabel(self.clicked)
+        self.show_image()
 
         hbox = QHBoxLayout(self)
-        hbox.addWidget(lbl)
+        hbox.addWidget(self.lbl)
         self.setLayout(hbox)
 
         self.move(100, 100)
@@ -51,11 +44,56 @@ class PicWindow(QWidget):
              filter='Image files (*.jpg)')
         return filename
 
-    def pixelize(self):
-        reply = QMessageBox.question(self, 'Message',
-                                     'PyQt5 message',
-                                     QtGui.QMessageBox.Yes,
-                                     QtGui.QMessageBox.No)
+    def clicked(self, x, y):
+        click_x = x
+        click_y = y
+
+        max_x = self.image.width
+        max_y = self.image.height
+
+        side = 10
+        averaging_side = 10
+
+        for bi in range(-side, side + 1):
+            for bj in range(-side, side + 1):
+                mixel_center_x = click_x + bi * averaging_side
+                mixel_center_y = click_y + bj * averaging_side
+                if mixel_center_x + averaging_side > max_x or \
+                                        mixel_center_x - averaging_side < 0 or \
+                                        mixel_center_y + averaging_side > max_y or \
+                                        mixel_center_y - averaging_side < 0:
+                    continue
+
+                mixel_width = range(mixel_center_x - averaging_side,
+                                    mixel_center_x + averaging_side)
+                mixel_height = range(mixel_center_y - averaging_side,
+                                     mixel_center_y + averaging_side)
+                area = len(mixel_width) * len(mixel_height)
+
+                r = 0
+                g = 0
+                b = 0
+                pix = self.image.load()
+                for i in mixel_width:
+                    for j in mixel_height:
+                        r = r + pix[i, j][0]
+                        g = g + pix[i, j][1]
+                        b = b + pix[i, j][2]
+                new_r = r // area
+                new_g = g // area
+                new_b = b // area
+
+                draw = ImageDraw.Draw(self.image)
+                for i in mixel_width:
+                    for j in mixel_height:
+                        draw.point((i, j), (new_r, new_g, new_b))
+
+        self.show_image()
+
+    def show_image(self):
+        img_tmp = ImageQt(self.image.convert('RGBA'))
+        pixmap = QPixmap.fromImage(img_tmp)
+        self.lbl.setPixmap(pixmap)
 
 
 if __name__ == '__main__':
